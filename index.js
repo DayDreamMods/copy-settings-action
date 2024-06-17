07c2accf4c23df5efc6a80b3ebf9d93e90a2b713
wwwDayDream <13383838+wwwDayDream@users.noreply.github.com>
2024-06-17 02:52:14 -0500

    Updated to support more refined settings by default

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const github = require('@actions/github');
 (async() => {
     const octokit = github.getOctokit(core.getInput('token'));
     const sourceRepo = core.getInput('source-repo');
-    const destRepo = core.getInput('dest-repo');
+    let destRepo = core.getInput("dest-repo");
+    if (destRepo.length == 0) 
+        destRepo = github.context.payload.repository.full_name;
     const settings = core.getInput('settings');
 
     const [sourceOwner, sourceRepoName] = sourceRepo.split('/');
@@ -18,13 +20,18 @@ const github = require('@actions/github');
 
     console.log("Obtained request data: \n" + JSON.stringify(sourceRequest.data));
 
-    const refinedSettings = [];
+    const refinedSettings = {};
     for (const setting of settings.split('\n')) {
         if (setting.length == 0) continue;
 
         const elem = sourceRequest.data[setting];
         if (elem == null) continue;
-        refinedSettings.push({ name: setting, value: elem });
+        refinedSettings[setting] = elem;
     }
     console.log(refinedSettings);
+
+    await octokit.rest.repos.update({
+        owner: destOwner, repo: destRepoName,
+        ...refinedSettings
+    });
 })();

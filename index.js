9aa9f1c31a46360543ec91d0dc248ded179d5791
wwwDayDream <13383838+wwwDayDream@users.noreply.github.com>
2024-06-17 02:09:41 -0500

    Initial logging

--- index.js
+++ index.js
@@ -3,5 +3,9 @@ const github = require('@actions/github');
 
 (async() => {
     const octokit = github.getOctokit(core.getInput('token'));
-    
+    const sourceRepo = core.getInput('source-repo');
+    const destRepo = core.getInput('dest-repo');
+    const settings = core.getInput('settings');
+
+    console.log(settings);
 })();

1999eaecb0896c5cbe6e14c0d6d741de773c3be0
wwwDayDream <13383838+wwwDayDream@users.noreply.github.com>
2024-06-17 02:24:58 -0500

    Logging and data parsing

--- index.js
+++ index.js
@@ -7,5 +7,24 @@ const github = require('@actions/github');
     const destRepo = core.getInput('dest-repo');
     const settings = core.getInput('settings');
 
-    console.log(settings);
+    const [sourceOwner, sourceRepoName] = sourceRepo.split('/');
+    const [destOwner, destRepoName] = destRepo.split('/');
+    
+    const sourceRequest = await github.rest.repos.get({
+        owner: sourceOwner, repo: sourceRepoName
+    });
+    if (sourceRequest.status != 200)
+        throw new Exception('Bad API response: ' + sourceRequest.status);
+
+    console.log("Obtained request data: \n" + JSON.stringify(sourceRequest.data));
+
+    const refinedSettings = [];
+    for (const setting of settings.split('\n')) {
+        if (setting.length == 0) continue;
+
+        const elem = sourceRequest.data[setting];
+        if (elem == null) continue;
+        refinedSettings.push({ name: setting, value: elem });
+    }
+    console.log(refinedSettings);
 })();
